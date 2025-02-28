6e281d591b410c64b324993e62bd02e6d1db96b6
JoaoPedroSSilva <97jpss@gmail.com>
2025-02-28 11:47:22 -0300

    Construtores classe ExpenseEntry

--- ExpenseControl/ExpenseControl/ExpenseEntry.cs
+++ ExpenseControl/ExpenseControl/ExpenseEntry.cs
@@ -2,12 +2,21 @@
 {
     internal class ExpenseEntry
     {
-        public int Id { get; private set; }
+        public static int Id { get; private set; }
         public DateTime DateEntry { get; private set; }
         public DateTime Date { get; set; }
         public ExpenseCategory Category { get; set; }
         public double Value { get; set; }
         public string Description { get; set; }
-        
+
+        public ExpenseEntry(DateTime date, ExpenseCategory category, double value, string description)
+        {
+            Id++;
+            DateEntry = DateTime.Now;
+            Date = date;
+            Category = category ?? throw new ArgumentNullException(nameof(category));
+            Value = value;
+            Description = description ?? throw new ArgumentNullException(nameof(description));
+        }
     }
 }
