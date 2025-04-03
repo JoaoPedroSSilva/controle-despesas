6760522d761b765979bc3326d8c70a0d7b5b886f
JoaoPedroSSilva <97jpss@gmail.com>
2025-04-03 15:06:42 -0300

    Implementado página de listagem de despesas

--- ExpenseControl/ExpenseControl/Views/ExpenseList.xaml.cs
+++ ExpenseControl/ExpenseControl/Views/ExpenseList.xaml.cs
@@ -1,3 +1,5 @@
+using ExpenseControl.Models;
+
 namespace ExpenseControl.Views;
 
 public partial class ExpenseList : ContentPage
@@ -5,5 +7,17 @@ public partial class ExpenseList : ContentPage
 	public ExpenseList()
 	{
 		InitializeComponent();
+		LoadExpenseList();
 	}
+
+    private async void LoadExpenseList()
+    {
+        List<ExpenseEntry> expensesList = await App.PersonRepo.GetAllExpenses();
+        expensesListView.ItemsSource = expensesList;
+    }
+
+    private async void OnRemoveExpenseClicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("Lançamento Excluído", "Lançamento excluído com sucesso.", "OK");
+    }
 }
